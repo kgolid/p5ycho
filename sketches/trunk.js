cf1bb5f65757e3df9597d9ab9ffe5facf5c3308a
Kjetil Golid <kjetil.golid@gmail.com>
2017-06-21 23:06:38 +0200

    add trunk sketch

---
+++ sketches/trunk.js
@@ -0,0 +1,56 @@
+let sketch = function(p) {
+
+  let dim_init = 40;
+  let dim_delta = 4;
+
+  let chaos_init = .5;
+  let chaos_delta = 0.12;
+  let chaos_mag = 18;
+
+  let ox = p.random(10000);
+  let oy = p.random(10000);
+  let oz = p.random(10000);
+
+  p.setup = function(){
+    p.createCanvas(1250, 800);
+    p.strokeWeight(1);
+    p.stroke(0);
+    p.smooth();
+    p.noFill();
+    p.noLoop();
+
+  }
+
+  p.draw = function() {
+    p.clear();
+    p.translate(p.width / 2, p.height / 2);
+    display(80);
+  }
+
+  function display(rings){
+
+    for(let i = 0; i < rings; i ++){
+    p.beginShape();
+      for(let angle = 0; angle < 360; angle++){
+        let radian = p.radians(angle);
+        let radius =  (chaos_mag * getNoiseWithTime(radian, chaos_delta * i + chaos_init)) + (dim_delta * i + dim_init);
+        p.vertex(radius * p.cos(radian), radius * p.sin(radian));
+      }
+    p.endShape(p.CLOSE);
+    }
+  }
+
+  function getNoise (radian, dim){
+    let r = radian % p.TWO_PI;
+    if(r < 0.0){r += p.TWO_PI;}
+    return p.noise(ox + p.cos(r) * dim, oy + p.sin(r) * dim);
+  }
+
+  function getNoiseWithTime (radian, dim, time){
+    let r = radian % p.TWO_PI;
+    if(r < 0.0){r += p.TWO_PI;}
+    return p.noise(ox + p.cos(r) * dim , oy + p.sin(r) * dim, oz + time);
+  }
+}
+
+new p5(sketch);
