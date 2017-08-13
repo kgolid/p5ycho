acdd6979ed5351e1acd936393d3d9b6f6ed3fd1b
Kjetil Golid <kjetil.golid@gmail.com>
2017-08-13 11:58:27 +0200

    further work in brick wall sketch

--- sketches/brick_walls.js
+++ sketches/brick_walls.js
@@ -1,62 +1,71 @@
 let sketch = function(p) {
-  let xdim = 4;
-  let ydim = 3;
-  let size = 20;
+  let xdim = 1;
+  let ydim = 1;
+  let size = 25;
 
   let vseps;
   let hseps;
 
   p.setup = function() {
-    p.createCanvas(1000,800);
-    p.frameRate(1);
+    p.createCanvas(1380,900);
+    p.frameRate(4);
+    p.noLoop();
     p.background(230,255,172);
-    p.fill(84,85,115);
-    p.stroke(230,255,172);
-    p.strokeWeight(2);
+    p.noFill();
   }
 
   p.draw = function() {
+    p.clear();
     p.translate(40,40);
-    for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 5; j++) {
-        generate_grid();
-        display();
-        p.translate(200, 0);
+    for (var i = 0; i < 5; i++) {
+      p.push();
+      for (var j = 0; j < 7; j++) {
+        generate_grid(xdim + j, ydim + i);
+
+        p.strokeWeight(8);
+        p.stroke(84,85,115);
+        display(xdim + j, ydim + i);
+        p.strokeWeight(4);
+        p.stroke(30,255,172);
+        display(xdim + j, ydim + i);
+
+        p.translate(120 + (j * size), 0);
       }
-      p.translate(-200 * 5, 200);
+      p.pop();
+      p.translate(0, 120 + (i * size));
     }
   }
 
-  function generate_grid() {
-    vseps = new Array(ydim);
-    hseps = new Array(ydim - 1);
+  function generate_grid(xd,yd) {
+    vseps = new Array(yd);
+    hseps = new Array(yd - 1);
 
-    for (var i = 0; i < ydim; i++) {
-      vseps[i] = new Array(xdim - 1);
+    for (var i = 0; i < yd; i++) {
+      vseps[i] = new Array(xd - 1);
       vseps[i].fill(-1);
     }
 
     vseps[0] = randomly_fill_remaining_vseps(vseps[0]);
 
-    for (var i = 0; i < ydim - 1; i++) {
-      hseps[i] = genereate_hseps(vseps[i], new Array(xdim));
+    for (var i = 0; i < yd - 1; i++) {
+      hseps[i] = genereate_hseps(vseps[i], new Array(xd));
       vseps[i + 1] = fill_forced_vseps(hseps[i], vseps[i], vseps[i + 1]);
       vseps[i + 1] = randomly_fill_remaining_vseps(vseps[i + 1]);
     }
   }
 
-  function display() {
+  function display(xd,yd) {
     p.push();
-    p.rect(0,0,xdim * size, ydim * size);
+    p.rect(0,0,xd * size, yd * size);
 
-    for (var j = 1; j < xdim; j++) {
-        if (vseps[0][j-1] === 1) p.line(j * size, 0, j * size, size);
-      }  
+    for (var j = 1; j < xd; j++) {
+      if (vseps[0][j-1] === 1) p.line(j * size, 0, j * size, size);
+    }  
 
-    for (var i = 0; i < ydim - 1; i++) {
+    for (var i = 0; i < yd - 1; i++) {
       p.translate(0, size);
       if (hseps[i][0] === 1) p.line(0, 0, size, 0);
-      for (var j = 1; j < xdim; j++) {
+      for (var j = 1; j < xd; j++) {
         if (vseps[i+1][j-1] === 1) p.line(j * size, 0, j * size, size);
         if (hseps[i][j] === 1)   p.line(j * size, 0, (j+1) * size, 0)
       }    
@@ -93,7 +102,7 @@ let sketch = function(p) {
   }
 
   function flip_coin() {
-    return p.random() < .5 ? 0:1
+    return p.random() < .6 ? 0:1
   }
 
 
