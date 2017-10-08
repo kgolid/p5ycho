63d5cd925bb46aa4a0f14b3b320e33f343a264a3
Kjetil Golid <kjetil.golid@gmail.com>
2017-10-08 17:23:30 +0200

    use lines instead of points

--- topology2/sketch.js
+++ topology2/sketch.js
@@ -5,12 +5,12 @@ let sketch = function(p) {
 
   let flow_cell_size = 5;
 
-  let noise_size = .06;
-  let noise_radius = .1;
+  let noise_size = 0.06;
+  let noise_radius = 0.1;
 
   let flow_width = (width + offset * 2) / flow_cell_size;
   let flow_height = (height + offset * 2) / flow_cell_size;
-  
+
   let noise_grid = [];
   let flow_grid = [];
 
@@ -20,39 +20,38 @@ let sketch = function(p) {
   let tick = 0;
   p.setup = function() {
     p.createCanvas(width, height);
-    p.background("#022");
+    p.background('#022');
     p.smooth();
     p.noStroke();
     //p.blendMode(p.OVERLAY);
 
     init_particles();
     init_flow();
-    
-  }
+  };
   p.draw = function() {
-    p.translate(-offset,-offset);
+    p.translate(-offset, -offset);
     //display_flow();
     update_particles();
     display_particles();
-    tick += .002;
-  }
+    tick += 0.002;
+  };
 
-  function init_particles () {
+  function init_particles() {
     for (var i = 0; i < number_of_particles; i++) {
       let r = p.random(p.width + 2 * offset);
       let q = p.random(p.height + 2 * offset);
       particles.push({
-          prev: p.createVector(r, q),
-          pos: p.createVector(r, q),
-          vel: p.createVector(0,0),
-          acc: p.createVector(0,0),
-          col: p.random(255),
-          seed: i
-        });
+        prev: p.createVector(r, q),
+        pos: p.createVector(r, q),
+        vel: p.createVector(0, 0),
+        acc: p.createVector(0, 0),
+        col: p.random(255),
+        seed: i
+      });
     }
   }
 
-  function update_particles () {
+  function update_particles() {
     for (var i = 0; i < number_of_particles; i++) {
       let prt = particles[i];
       let flow = get_flow(prt.pos.x, prt.pos.y);
@@ -60,39 +59,42 @@ let sketch = function(p) {
       prt.prev.x = prt.pos.x;
       prt.prev.y = prt.pos.y;
 
-      prt.pos.x = mod((prt.pos.x + prt.vel.x), p.width + 2 * offset);
-      prt.pos.y = mod((prt.pos.y + prt.vel.y), p.height + 2 * offset);
+      prt.pos.x = mod(prt.pos.x + prt.vel.x, p.width + 2 * offset);
+      prt.pos.y = mod(prt.pos.y + prt.vel.y, p.height + 2 * offset);
 
-      prt.vel.add(prt.acc).normalize().mult(2);
-      
+      prt.vel
+        .add(prt.acc)
+        .normalize()
+        .mult(2);
+
       //prt.acc = p5.Vector.fromAngle(p.noise(prt.seed * 10, tick) * p.TAU).mult(0.01);
-      prt.acc = p.createVector(0,0);
+      prt.acc = p.createVector(0, 0);
       prt.acc.add(flow).mult(5);
     }
   }
 
-  function init_flow () {
-    for(let i = 0; i < flow_height; i++) {
-      let row = []
-      for(let j = 0; j < flow_width; j++) {
+  function init_flow() {
+    for (let i = 0; i < flow_height; i++) {
+      let row = [];
+      for (let j = 0; j < flow_width; j++) {
         row.push(calculate_flow(j * noise_size, i * noise_size, noise_radius));
       }
       flow_grid.push(row);
     }
   }
 
-  function calculate_flow (x,y,r) {
+  function calculate_flow(x, y, r) {
     //console.log(x,y);
     let high_val = 0;
     let low_val = 1;
-    let high_pos = p.createVector(0,0);
-    let low_pos = p.createVector(0,0);
+    let high_pos = p.createVector(0, 0);
+    let low_pos = p.createVector(0, 0);
 
-    for(var i = 0; i < 100; i++) {
-      let angle = (i / 100) * p.TAU;
-      let pos = p.createVector(x + (p.cos(angle) * r), y + (p.sin(angle) * r))
-      let val = p.noise(pos.x,pos.y);
-      
+    for (var i = 0; i < 100; i++) {
+      let angle = i / 100 * p.TAU;
+      let pos = p.createVector(x + p.cos(angle) * r, y + p.sin(angle) * r);
+      let val = p.noise(pos.x, pos.y);
+
       if (val > high_val) {
         high_val = val;
         high_pos.x = pos.x;
@@ -111,42 +113,48 @@ let sketch = function(p) {
     return flow_angle;
   }
 
-  function get_flow(xpos,ypos) {
+  function get_flow(xpos, ypos) {
     xpos = p.constrain(xpos, 0, p.width + 2 * offset);
     ypos = p.constrain(ypos, 0, p.height + 2 * offset);
-    return flow_grid[p.floor(ypos / flow_cell_size)][p.floor(xpos / flow_cell_size)]
+    return flow_grid[p.floor(ypos / flow_cell_size)][p.floor(xpos / flow_cell_size)];
   }
 
-  function display_particles () {
+  function display_particles() {
     p.strokeWeight(2);
-    p.stroke(255,240,220,7);
-    for(let i = 0; i < particles.length; i++) {
+    p.stroke(255, 240, 220, 5);
+    for (let i = 0; i < particles.length; i++) {
       //p.stroke(particles[i].col);
-      p.point(particles[i].pos.x, particles[i].pos.y);
-      //p.line(particles[i].prev.x, particles[i].prev.y, particles[i].pos.x, particles[i].pos.y);
+      //p.point(particles[i].pos.x, particles[i].pos.y);
+      if (p5.Vector.dist(particles[i].prev, particles[i].pos) < 20)
+        p.line(particles[i].prev.x, particles[i].prev.y, particles[i].pos.x, particles[i].pos.y);
     }
   }
 
-  function display_flow () {
-    for(let i = 0; i < flow_grid.length; i++) {
-      for(let j = 0; j < flow_grid[i].length; j++) {
+  function display_flow() {
+    for (let i = 0; i < flow_grid.length; i++) {
+      for (let j = 0; j < flow_grid[i].length; j++) {
         p.strokeWeight(1);
-        p.stroke(255,0,0);
+        p.stroke(255, 0, 0);
         p.noFill();
-        p.ellipse((j) * flow_cell_size, (i) * flow_cell_size, 7, 7);
-        p.line((j) * flow_cell_size, (i) * flow_cell_size, (j) * flow_cell_size + flow_grid[i][j].x * 50, (i) * flow_cell_size  + flow_grid[i][j].y * 50);
+        p.ellipse(j * flow_cell_size, i * flow_cell_size, 7, 7);
+        p.line(
+          j * flow_cell_size,
+          i * flow_cell_size,
+          j * flow_cell_size + flow_grid[i][j].x * 50,
+          i * flow_cell_size + flow_grid[i][j].y * 50
+        );
       }
     }
   }
 
-  p.keyPressed = function () {
+  p.keyPressed = function() {
     if (p.keyCode === 80) {
-      p.saveCanvas("landslide", "jpeg");
+      p.saveCanvas('landslide', 'jpeg');
     }
-  }
+  };
 
-  function mod (x, n) {
-    return ((x % n) + n ) % n;
+  function mod(x, n) {
+    return (x % n + n) % n;
   }
-}
+};
 new p5(sketch);
