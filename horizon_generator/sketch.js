7f8f8ce1153d2e9d4978290fc10a47ead0ea79fc
Kjetil Golid <kjetil.golid@gmail.com>
2017-10-15 10:56:53 +0200

    added better controllers to horizon generator

--- horizon_generator/sketch.js
+++ horizon_generator/sketch.js
@@ -4,45 +4,36 @@ let sketch = function(p) {
   let deviation = 100;
   let number_of_interpolations = 8;
   let number_of_layers = 6;
+  let shapes_per_layer = 45;
 
   let points;
   let current;
 
   let filename = 'landslide';
-  let auto_generate = false;
-  let auto_download = false;
 
-  let auto_generate_checkbox, auto_download_checkbox;
+  let use_custom_color_seed = false;
+  let custom_color_seed = 94938984;
 
-  let use_custom_seed = false;
-  let custom_seed = 94938984;
+  let use_custom_shape_seed = false;
+  let custom_shape_seed = 94938984;
 
-  let use_custom_palette = false;
+  let use_saved_palette = false;
+  let current_palette = [];
+  let random_palette = [];
 
-  let use_previous_hues = false;
-  let current_hues = [];
   let hue_inputs = [];
+  let shape_input = 0;
+  let use_this_palette_checkbox, use_this_shape_checkbox;
 
-  let THE_SEED;
-
-  let palettes = {
-    hieronymus: [141, 20, 350, 208, 31, 96],
-    sunset: [265, 6, 325, 38, 255, 107],
-    fiery: [14, 234, 27, 356, 248, 53],
-    blue_mountain: [60, 241, 189, 246, 216, 185],
-    purple_green: [44, 1, 166, 286, 165, 80],
-    orange_purple: [335, 54, 198, 312, 91, 261],
-    lavarock: [200, 32, 359, 27, 208, 187],
-    dusk: [206, 61, 51, 224, 26, 40],
-    savannah: [36, 128, 30, 355, 271, 20]
-  };
+  let COLOR_SEED;
+  let SHAPE_SEED;
 
   p.setup = function() {
     var canvas = p.createCanvas(2000, 1200);
     canvas.parent('sketch');
-    p.noStroke();
     p.colorMode(p.HSB);
     p.blendMode(p.MULTIPLY);
+    p.noStroke();
     p.noLoop();
 
     setup_controllers();
@@ -50,14 +41,40 @@ let sketch = function(p) {
 
   p.draw = function() {
     set_parametres();
-
-    if (auto_download) saveToFile();
-
-    THE_SEED = p.floor(p.random(99999999));
-    p.randomSeed(THE_SEED);
-
+    set_seeds();
+    p.randomSeed(COLOR_SEED);
+    set_colors();
+    p.randomSeed(SHAPE_SEED);
     display();
   };
+
+  function set_seeds() {
+    // Using Math.random over P5.random to avoid the P5 seed.
+    COLOR_SEED = use_custom_color_seed ? custom_color_seed : p.floor(Math.random() * 100000000);
+    SHAPE_SEED = use_custom_shape_seed ? custom_shape_seed : p.floor(Math.random() * 100000000);
+  }
+
+  function set_colors() {
+    random_palette = [];
+    for (var i = 0; i < number_of_layers; i++) {
+      random_palette.push(p.random(360));
+    }
+  }
+
+  function display() {
+    p.clear();
+    p.background('#fff');
+    for (var h = 0; h < number_of_layers; h++) {
+      init(h * 250 - 100);
+      let hue = use_saved_palette ? current_palette[h] : random_palette[h];
+      current_palette[h] = hue;
+      p.fill(hue, 100, 95, 0.012);
+      for (var i = 0; i < shapes_per_layer; i++) {
+        let shape = get_variant();
+        display_shape(shape);
+      }
+    }
+  }
 
   function init(ypos) {
     points = [];
@@ -71,39 +88,25 @@ let sketch = function(p) {
     }
   }
 
-  function update() {
+  function get_variant() {
     let c = deep_copy(points);
     for (let b = 0; b < 8; b++) {
-      for (let i = 0; i < c.length; i++) {
-        move_nearby(c[i], deviation);
-      }
+      c.forEach(function(pnt) {
+        move_nearby(pnt, deviation);
+      }, this);
     }
     return c;
   }
 
-  function display() {
-    p.clear();
-    p.background('#fff');
-    for (var h = 0; h < number_of_layers; h++) {
-      init(h * 250 - 100);
-      let hue = use_custom_palette ? current_hues[h] : p.random(360);
-      current_hues[h] = hue;
-      console.log(hue);
-      p.fill(hue, 100, 95, 0.012);
-      for (var i = 0; i < 45; i++) {
-        current = update();
-        display_row();
-      }
-    }
-  }
-
-  function display_row() {
+  function display_shape(shape) {
     p.beginShape();
-    p.vertex(0, current[0].y);
-    for (let i = 0; i < current.length; i++) {
-      p.vertex(current[i].x, current[i].y);
-    }
-    p.vertex(p.width, current[current.length - 1].y);
+    p.vertex(0, shape[0].y);
+
+    shape.forEach(function(pnt) {
+      p.vertex(pnt.x, pnt.y);
+    });
+
+    p.vertex(p.width, shape[shape.length - 1].y);
     p.vertex(p.width, p.height);
     p.vertex(0, p.height);
     p.endShape(p.CLOSE);
@@ -135,40 +138,52 @@ let sketch = function(p) {
   };
 
   let saveToFile = function() {
-    p.saveCanvas(filename + '_' + THE_SEED, 'jpeg');
+    p.saveCanvas(filename + '_c' + COLOR_SEED + '_s' + SHAPE_SEED, 'jpeg');
   };
 
-  // ----
+  // ---- UI ----
 
   let setup_controllers = function() {
-    var generate_button = p.createButton('Generate (G)').parent('controller');
-    var download_button = p.createButton('Download (P)').parent('controller');
-
-    var palette_container = p.createDiv('Color palette: ').parent('controller');
-    var get_palette_button = p.createButton('Get palette from image').parent(palette_container);
+    let palette_container = p.createDiv('').parent('controller');
+    let get_palette_button = p.createButton('Get palette from image').parent(palette_container);
     for (var i = 0; i < number_of_layers; i++) {
-      hue_inputs.push(p.createInput(palettes['dusk'][i], 'number').parent(palette_container));
+      hue_inputs.push(p.createInput(i * 40 + 20, 'number').parent(palette_container));
     }
     use_this_palette_checkbox = p.createCheckbox('Use this palette', false).parent(palette_container);
 
+    let shape_container = p.createDiv('').parent('controller');
+    let get_shape_button = p.createButton('Get shape from image').parent(shape_container);
+    shape_input = p.createInput(0, 'number').parent(shape_container);
+    use_this_shape_checkbox = p.createCheckbox('Use this shape', false).parent(shape_container);
+
+    let action_container = p.createDiv('').parent('controller');
+    let generate_button = p.createButton('Generate (G)').parent(action_container);
+    let download_button = p.createButton('Download (P)').parent(action_container);
+
+    get_palette_button.mousePressed(update_hue_inputs);
+    get_shape_button.mousePressed(update_shape_input);
     generate_button.mousePressed(p.draw);
     download_button.mousePressed(saveToFile);
-    get_palette_button.mousePressed(update_hue_inputs);
-
-    set_parametres();
   };
 
   let update_hue_inputs = function() {
-    for (var i = 0; i < number_of_layers; i++) {
-      hue_inputs[i].value(p.floor(current_hues[i]));
-    }
+    hue_inputs.forEach(function(hue_input, i) {
+      hue_input.value(p.floor(current_palette[i]));
+    }, this);
+  };
+
+  let update_shape_input = function() {
+    shape_input.value(SHAPE_SEED);
   };
 
   let set_parametres = function() {
     for (var i = 0; i < number_of_layers; i++) {
-      current_hues[i] = p.constrain(hue_inputs[i].value(), 0, 360);
+      current_palette[i] = p.constrain(hue_inputs[i].value(), 0, 360);
     }
-    use_custom_palette = use_this_palette_checkbox.checked();
+    custom_shape_seed = shape_input.value();
+
+    use_custom_shape_seed = use_this_shape_checkbox.checked();
+    use_saved_palette = use_this_palette_checkbox.checked();
   };
 
   p.keyPressed = function() {
