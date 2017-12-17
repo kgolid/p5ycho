b2b8c4e67ec4894fadbe56d88341ae2e37cdbb23
Kjetil Golid <kjetil.golid@gmail.com>
2017-12-18 00:49:18 +0100

    add new interpolation sketch

---
+++ interpolate/sketch.js
@@ -0,0 +1,58 @@
+let sketch = function(p) {
+  let THE_SEED;
+
+  let cdimx = 2;
+  let cdimy = 3;
+  let cgrid;
+
+  let dimx = 200;
+  let dimy = 200;
+
+  let pad = 50;
+
+  p.setup = function() {
+    p.createCanvas(1000, 1000);
+    THE_SEED = p.floor(p.random(9999999));
+    p.randomSeed(THE_SEED);
+    p.noStroke();
+    p.noLoop();
+
+    cgrid = [];
+    for (var i = 0; i < cdimy; i++) {
+      crow = [];
+      for (var j = 0; j < cdimx; j++) {
+        crow.push(p.color(p.random(255), p.random(255), p.random(255), p.random(255)));
+      }
+      cgrid.push(crow);
+    }
+  };
+
+  p.draw = function() {
+    p.background('#222');
+    for (var y = 0; y < dimy; y++) {
+      for (var x = 0; x < dimx; x++) {
+        let px = x / dimx * (cdimx - 1);
+        let py = y / dimy * (cdimy - 1);
+
+        let px0 = p.floor(px);
+        let py0 = p.floor(py);
+
+        let sx = p.map(px, px0, px0 + 1, 0, 1);
+        let sy = p.map(py, py0, py0 + 1, 0, 1);
+
+        let cu = p.lerpColor(cgrid[py0][px0], cgrid[py0][px0 + 1], sx);
+        let cl = p.lerpColor(cgrid[py0 + 1][px0], cgrid[py0 + 1][px0 + 1], sx);
+        let c = p.lerpColor(cu, cl, sy);
+        p.fill(c);
+        let w = (p.width - pad * 2) / dimx;
+        let h = (p.height - pad * 2) / dimy;
+        p.rect(pad + w * x, pad + h * y, w, h);
+      }
+    }
+  };
+
+  p.keyPressed = function() {
+    if (p.keyCode === 80) p.saveCanvas('sketch_' + THE_SEED, 'jpeg');
+  };
+};
+new p5(sketch);
