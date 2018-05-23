cb8c3d4cc57c6ba1678e6fa6cc6887104a6cdc14
Kjetil Golid <kjetil.golid@gmail.com>
2018-05-23 18:41:07 +0200

    update elements with intersection function

--- utils/elementsjs/index.js
+++ utils/elementsjs/index.js
@@ -55,3 +55,24 @@ export function point_at_distance_and_angle(p1, dist, rad) {
 export function point_at_distance_towards_direction(p1, dist, pdir) {
   return point_at_distance_and_angle(p1, dist, angle_of_direction(p1, pdir));
 }
+
+export function intersection_of_two_circles(p1, r1, p2, r2) {
+  let d = distance_between_points(p1, p2);
+
+  // Circles are seperate.
+  if (d > r1 + r2) return [];
+  // One circle is contained within the other.
+  if (d < Math.abs(r1 - r2)) return [];
+  // Circles are coincident.
+  if (d === 0 && r1 === r2) return [];
+
+  let a = (Math.pow(r1, 2) - Math.pow(r2, 2) + Math.pow(d, 2)) / (2 * d);
+  let b =
+    Math.sqrt(4 * Math.pow(d, 2) * Math.pow(r1, 2) - Math.pow(Math.pow(d, 2) - Math.pow(r2, 2) + Math.pow(r1, 2), 2)) /
+    d;
+
+  let p1p2_angle = angle_of_direction(p1, p2);
+  let ab_angle = angle_of_direction([0, 0], [a, b]);
+
+  return point_at_distance_and_angle(p1, r1, p1p2_angle + ab_angle);
+}
